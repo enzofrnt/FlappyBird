3cbd561af8bed2fdbc70624c244c84b818306751
Fournet Enzo <enzofournet31@gmail.com>
2025-01-19 14:48:29 +0100

    server prof fixe

--- game_score_server/server/settings/production.py
+++ game_score_server/server/settings/production.py
@@ -11,56 +11,8 @@ DATABASES = {
     }
 }
 
-# Configuration des logs pour la production
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': '[{asctime}] {levelname} [{name}:{lineno}] {message}',
-            'style': '{',
-            'datefmt': '%d/%b/%Y %H:%M:%S'
-        },
-    },
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose',
-            'level': 'DEBUG',  # Changé à DEBUG pour voir plus de détails
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': 'DEBUG',  # Changé à DEBUG
-            'propagate': True,
-        },
-        'django.server': {  # Ajout des logs du serveur
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-        'gamescore': {
-            'handlers': ['console'],
-            'level': 'DEBUG',  # Changé à DEBUG
-            'propagate': True,
-        },
-        'gunicorn.access': {
-            'handlers': ['console'],
-            'level': 'DEBUG',  # Changé à DEBUG
-            'propagate': False,
-        },
-        'gunicorn.error': {
-            'handlers': ['console'],
-            'level': 'DEBUG',  # Changé à DEBUG
-            'propagate': False,
-        },
-    },
-} 
 
 
-REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES': [
-        'rest_framework.renderers.JSONRenderer',
-    ],
-}
+REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
+    'rest_framework.renderers.JSONRenderer',
+]
