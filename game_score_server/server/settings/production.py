d6d670ad86613c0e384c2b42cfd72ff9f1d73be5
Fournet Enzo <enzofournet31@gmail.com>
2025-01-19 12:37:49 +0100

    server logs

--- game_score_server/server/settings/production.py
+++ game_score_server/server/settings/production.py
@@ -1,7 +1,8 @@
 from .base import *
+import os
 
 DEBUG = False
-ALLOWED_HOSTS = ['your-domain.com']  # À modifier avec votre domaine
+ALLOWED_HOSTS = ['192.168.1.206']  # À modifier avec votre domaine
 
 DATABASES = {
     "default": {
@@ -13,4 +14,57 @@ DATABASES = {
 # Configuration de sécurité supplémentaire
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True 
+CSRF_COOKIE_SECURE = True
+
+# Configuration des logs pour la production
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '[{asctime}] {levelname} [{name}:{lineno}] {message}',
+            'style': '{',
+            'datefmt': '%d/%b/%Y %H:%M:%S'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs', 'django-prod.log'),
+            'maxBytes': 1024 * 1024 * 5,  # 5 MB
+            'backupCount': 5,
+            'formatter': 'verbose',
+        },
+        'gunicorn': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs', 'gunicorn-prod.log'),
+            'maxBytes': 1024 * 1024 * 5,  # 5 MB
+            'backupCount': 5,
+            'formatter': 'verbose',
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        'gamescore': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        'gunicorn.access': {
+            'handlers': ['gunicorn'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+        'gunicorn.error': {
+            'handlers': ['gunicorn'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
+} 
